c69168116b9b055e27a8eb395dfdf842894b79eb
aliadnaan <madnaanali37@gmail.com>
2025-10-12 01:29:28 -0700

    fixed sim

--- Part1_DigitalLogic/project3_fixed/project3_fixed.srcs/sources_1/new/majority_of_five.v
+++ Part1_DigitalLogic/project3_fixed/project3_fixed.srcs/sources_1/new/majority_of_five.v
@@ -20,9 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module majority_of_five(input [7:0] sw, output led);
+module majority_of_five(input [4:0] sw, output led);
 
-assign led =	(sw[7] & ~sw[6] & sw[5] & ~sw[4]) | (sw[7] & ~sw[6] & sw[5] & sw[4] & ~sw[3]) | (sw[7] & ~sw[6] & sw[5] & sw[4] & sw[3] & (~sw[2] & ~sw[1] & ~sw[0]));
-
+assign led =	(sw[0] & sw[1] & sw[2]) | //ABC
+		(sw[0] & sw[1] & sw[3]) | //ABD
+		(sw[0] & sw[1] & sw[4]) | //ABE
+		(sw[0] & sw[2] & sw[3]) | //ACD
+		(sw[0] & sw[2] & sw[4]) | //ACE
+		(sw[0] & sw[3] & sw[4]) | //ADE
+		(sw[1] & sw[2] & sw[3]) | //BCD
+		(sw[1] & sw[2] & sw[4]) | //BCE
+		(sw[1] & sw[3] & sw[4]) | //BDE
+		(sw[2] & sw[3] & sw[4]);  //CDE
 		
 endmodule
